88dedb67151e2efc74166277bcfc6a690ba7fb78
Mic <michelelt660@gmail.com>
2022-07-31 17:21:36 +0200

    improved fetch/api/table functionality

---
+++ apiTable.js
@@ -0,0 +1,163 @@
+const container = document.querySelector("tbody");
+const bottone = document.querySelector("#genera-news");
+let dat = [];
+
+//fetch api and create pagination
+async function fetchArticoloSito() {
+  const response = await fetch(
+    `https://api.spaceflightnewsapi.net/v3/articles?_limit=-1`
+  );
+  const element = await response.json();
+  dat = element;
+
+  //create select element with News Site of API
+  let siteList = [];
+  dat.forEach((element) => {
+    if (!siteList.includes(element.newsSite)) {
+      siteList.push(element.newsSite);
+      const tag = document.createElement("option");
+      document.querySelector(".dropdown-content").appendChild(tag);
+      tag.innerHTML = element.newsSite;
+      tag.setAttribute("value", element.newsSite);
+    }
+  });
+
+  //pagination
+  function template(el) {
+    el.forEach((element) => {
+      // if(value == element.newsSite || value == 'qualsiasi') {
+      let card = document.createElement("tr");
+      container.appendChild(card);
+      card.innerHTML = `
+                 <td>${element.publishedAt.slice(0, 10)}</td>
+                 <td>${element.title}</td>
+                 <td><a href = "${
+                   element.url
+                 }" target = '_blank'>Read more</a></td>
+                 <td><a class="btn btn-primary bla2" data-toggle="popover" data-bs-trigger="focus" 
+                 data-img="${element.imageUrl}" href=${
+        element.imageUrl
+      } target="_blank">View Image</a></td>
+                 `;
+      $('[data-toggle="popover"]').popover({
+        html: true,
+        trigger: "hover",
+        placement: "left",
+        content: function () {
+          return (
+            '<img class="bla" src="' +
+            $(this).data("img") +
+            '" style="width:200px;" />'
+          );
+        },
+      });
+    });
+  }
+
+  $("table").pagination({
+    dataSource: dat,
+    totalNumber: dat.length,
+    pageSize: 30,
+    callback: function (data, a) {
+      // template method of yourself
+      container.innerHTML = "";
+      var html = template(data);
+      $("tbody").html(html);
+    },
+  });
+}
+
+fetchArticoloSito();
+
+//create pagination and filter news by News site in select element
+let testSelect = document.querySelector(".dropdown-content");
+testSelect.addEventListener("change", () => {
+  container.innerHTML = "";
+  let datfilter = [];
+  dat.forEach((el) => {
+    if (el.newsSite == testSelect.value) {
+      datfilter.push(el);
+    }
+  });
+
+  function template(el) {
+    el.forEach((element) => {
+      let card = document.createElement("tr");
+      container.appendChild(card);
+      card.innerHTML = `
+               <td>${element.publishedAt.slice(0, 10)}</td>
+               <td>${element.title}</td>
+               <td><a href = "${
+                 element.url
+               }" target = '_blank'>Read more</a></td>
+               <td><a class="btn btn-primary bla2" data-toggle="popover" data-bs-trigger="focus" 
+               data-img="${element.imageUrl}" href=${
+        element.imageUrl
+      } target="_blank">View Image</a></td>
+               `;
+
+      $('[data-toggle="popover"]').popover({
+        html: true,
+        trigger: "hover",
+        placement: "left",
+        content: function () {
+          return (
+            '<img class="bla" src="' +
+            $(this).data("img") +
+            '" style="width:200px;" />'
+          );
+        },
+      });
+    });
+  }
+
+  $("table").pagination({
+    dataSource: datfilter,
+    totalNumber: datfilter.length,
+    pageSize: 30,
+    callback: function (data, a) {
+      // template method of yourself
+      container.innerHTML = "";
+      var html = template(data);
+      $("tbody").html(html);
+    },
+  });
+});
+
+//blog
+async function createListTagBlog() {
+  const response = await fetch(
+    "https://api.spaceflightnewsapi.net/v3/blogs?_limit=100"
+  );
+  const element = await response.json();
+  let siteList = [];
+  element.forEach((element) => {
+    if (!siteList.includes(element.newsSite)) {
+      siteList.push(element.newsSite);
+      const tag = document.createElement("option");
+      document.querySelector(".dropdown-content").appendChild(tag);
+      tag.innerHTML = element.newsSite;
+      tag.setAttribute("value", element.newsSite);
+    }
+  });
+  console.log(siteList);
+}
+
+//reports
+async function createListTagReports() {
+  const response = await fetch(
+    "https://api.spaceflightnewsapi.net/v3/reports?_limit=100"
+  );
+  const element = await response.json();
+  let siteList = [];
+  element.forEach((element) => {
+    if (!siteList.includes(element.newsSite)) {
+      siteList.push(element.newsSite);
+      const tag = document.createElement("option");
+      document.querySelector(".dropdown-content").appendChild(tag);
+      tag.innerHTML = element.newsSite;
+      tag.setAttribute("value", element.newsSite);
+    }
+  });
+  console.log(siteList);
+}
